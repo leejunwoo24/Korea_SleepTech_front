b962bef723988c0324191af335d8c5092e616a82
leejunwoo24 <yusm8758@naver.com>
2025-04-02 12:06:46 +0900

    20250402

--- korea-sleepTach-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTach-react/src/pages/b_hooks/index.tsx
@@ -9,6 +9,11 @@ import UseStatePractice from "./a_useState/Practice";
 import UseRef01 from "./b_useRef/UseRef01";
 import UseRef02 from "./b_useRef/UseRef02";
 import UseRefPractice from "./a_useState/Practice";
+import UseEffect01 from "./c_useEffect/UseEffect01";
+import UseEffect02 from "./c_useEffect/UseEffect02";
+import UseCallback from "./d_useCallback_useMemo/UseCallback";
+import UseMemo from "./d_useCallback_useMemo/UseMemo";
+import ReactMemo from "./d_useCallback_useMemo/ReactMemo";
 
 const h2Style = {
   backgroundColor: "black",
@@ -26,6 +31,15 @@ function Index() {
       >
         === 리액트 Hooks ===
       </h1>
+
+        <h2 style={h2Style}>리액트 Hooks - useCallback</h2>
+        <UseCallback/>
+        <UseMemo/>
+        <ReactMemo />
+
+      <h2 style={h2Style}>리액트 Hooks - useEffect</h2>
+      <UseEffect01/><hr />
+      <UseEffect02/>
 
       <h2 style={h2Style}>리액트 Hooks - useRef</h2>
       <UseRef01 /> <hr />
